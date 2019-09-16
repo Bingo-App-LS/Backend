a1c5e1d1df04cb40c4b620b182436fa32b848db8
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-09-15 21:01:34 -0500

    fixed error in migrations

--- database/migrations/20190914160223_shuffledboard.js
+++ database/migrations/20190914160223_shuffledboard.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('shuffledboard', board => {
         board.increments();
-        board.integer('user_id').notNullabe().references('id').inTable('users').onDelete('cascade')
+        board.integer('user_id').notNullable().references('id').inTable('users').onDelete('cascade')
         board.integer('game_id').notNullable().references('id').inTable('game').onDelete('cascade')
         board.specificType('phrases', 'varchar[]');
     })
