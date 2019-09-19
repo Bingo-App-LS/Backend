48e0e43d1ca9c8534f1ab895154cf1aecfb6884e
AI2EJAY <39928154+AI2EJAY@users.noreply.github.com>
2019-09-19 03:46:41 -0500

    fixed gitignore

---
+++ api/passport.js
@@ -0,0 +1,24 @@
+// Boiler plate code from passport docs dealing with the facebook strategy. 
+// We need to setup facebook dev & link our app before we can get too deep into it
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
